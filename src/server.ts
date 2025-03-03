a8010496075e79e88c5a9a29a781dfef9fc5becd
Jan Adrian Hafskjold <jahafskj@stud.ntnu.no>
2025-03-03 20:40:51 +0100

    Tighten CORS-policy

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,17 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
-// Load environment variables from .env file
 dotenv.config();
 
-// Create express app
 const app = express();
-
-// Parse request body as JSON
 app.use(express.json());
 
-// Enable CORS and allow all origins
 app.use(
   cors({
-    origin: "*",
+    origin: "http://localhost:5173", // Local vite
   })
 );
 
-// Define port and start server
 const PORT: number = Number(process.env.PORT || 5001);
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
