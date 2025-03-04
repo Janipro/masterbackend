5a6481233ab5fb5b371ee99cd5b1169350cda29d
Jan Adrian Hafskjold <jahafskj@stud.ntnu.no>
2025-03-04 19:07:34 +0100

    Plural

--- database.sql
+++ database.sql
@@ -1,29 +1,29 @@
-CREATE TABLE Requirement (
+CREATE TABLE Requirements (
     requirement_id int NOT NULL,
 	requirement_name varchar(255),
 	PRIMARY KEY (requirement_id)
 );
 
-CREATE TABLE "Class" (
+CREATE TABLE Classes (
     class_id int NOT NULL,
     grade int,
 	class_name varchar(10),
     PRIMARY KEY (class_id)
 );
 
-CREATE TABLE School (
+CREATE TABLE Schools (
 	school_id int NOT NULL,
 	school_name varchar(255),
 	PRIMARY KEY (school_id)
 );
 
-CREATE TABLE Course (
+CREATE TABLE Courses (
     course_id int NOT NULL,
     course_name varchar(255),
     PRIMARY KEY (course_id)
 );
 
-CREATE TABLE "User" (
+CREATE TABLE Users (
     user_id int NOT NULL,
     firstname varchar(255),
     lastname varchar(255),
@@ -34,11 +34,11 @@ CREATE TABLE "User" (
 	password_hash text,
 	created_at Date,
     PRIMARY KEY (user_id),
-    FOREIGN KEY (class_id) REFERENCES "Class"(class_id),
-	FOREIGN KEY (school_id) REFERENCES School(school_id)
+    FOREIGN KEY (class_id) REFERENCES Classes(class_id),
+	FOREIGN KEY (school_id) REFERENCES Schools(school_id)
 );
 
-CREATE TABLE StudyGroup (
+CREATE TABLE StudyGroups (
 	study_group_id int NOT NULL,
 	course_id int,
 	school_id int,
@@ -47,30 +47,30 @@ CREATE TABLE StudyGroup (
 	user_id int,
 	is_active boolean,
 	PRIMARY KEY (study_group_id),
-	FOREIGN KEY (course_id) REFERENCES Course(course_id),
-	FOREIGN KEY (school_id) REFERENCES School(school_id),
-	FOREIGN KEY (user_id) REFERENCES "User"(user_id)
+	FOREIGN KEY (course_id) REFERENCES Courses(course_id),
+	FOREIGN KEY (school_id) REFERENCES Schools(school_id),
+	FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
 
-CREATE TABLE Enrolment (
+CREATE TABLE Enrolments (
     enrolment_id int NOT NULL,
 	user_id int,
 	study_group_id int,
 	PRIMARY KEY (enrolment_id),
-	FOREIGN KEY (user_id) REFERENCES "User"(user_id),
-	FOREIGN KEY (study_group_id) REFERENCES StudyGroup(study_group_id)
+	FOREIGN KEY (user_id) REFERENCES Users(user_id),
+	FOREIGN KEY (study_group_id) REFERENCES StudyGroups(study_group_id)
 );
 
-CREATE TABLE "Access" (
+CREATE TABLE Access (
 	access_id int NOT NULL,
 	user_id int,
 	study_group_id int,
 	PRIMARY KEY (access_id),
-	FOREIGN KEY (user_id) REFERENCES "User"(user_id),
-	FOREIGN KEY (study_group_id) REFERENCES StudyGroup(study_group_id)
+	FOREIGN KEY (user_id) REFERENCES Users(user_id),
+	FOREIGN KEY (study_group_id) REFERENCES StudyGroups(study_group_id)
 );
 
-CREATE TABLE Task (
+CREATE TABLE Tasks (
     task_id int NOT NULL,
 	task_name varchar(255),
 	task_description varchar(255),
@@ -83,17 +83,17 @@ CREATE TABLE Task (
 	image_url text,
 	is_active boolean,
 	PRIMARY KEY (task_id),
-	FOREIGN KEY (course_id) REFERENCES Course(course_id),
-	FOREIGN KEY (user_id) REFERENCES "User"(user_id)
+	FOREIGN KEY (course_id) REFERENCES Courses(course_id),
+	FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );	
 
-CREATE TABLE TaskRequirement (
+CREATE TABLE TaskRequirements (
 	task_requirement_id int NOT NULL,
 	task_id int,
 	requirement_id int,
 	PRIMARY KEY (task_requirement_id),
-	FOREIGN KEY (task_id) REFERENCES Task(task_id),
-	FOREIGN KEY (requirement_id) REFERENCES Requirement(requirement_id)
+	FOREIGN KEY (task_id) REFERENCES Tasks(task_id),
+	FOREIGN KEY (requirement_id) REFERENCES Requirements(requirement_id)
 );
 
 CREATE TABLE Recommended (
@@ -101,11 +101,11 @@ CREATE TABLE Recommended (
 	user_id int,
 	task_id int,
 	PRIMARY KEY (recommended_id),
-	FOREIGN KEY (user_id) REFERENCES "User"(user_id),
-	FOREIGN KEY (task_id) REFERENCES Task(task_id)
+	FOREIGN KEY (user_id) REFERENCES Users(user_id),
+	FOREIGN KEY (task_id) REFERENCES Tasks(task_id)
 );
 
-CREATE TABLE Submission (
+CREATE TABLE Submissions (
     submission_id int NOT NULL,
 	task_id int,
 	user_id int,
@@ -113,17 +113,17 @@ CREATE TABLE Submission (
 	generated_output varchar(255),
 	gpt_feedback text,
 	PRIMARY KEY (submission_id),
-	FOREIGN KEY (task_id) REFERENCES Task(task_id),
-	FOREIGN KEY (user_id) REFERENCES "User"(user_id)
+	FOREIGN KEY (task_id) REFERENCES Tasks(task_id),
+	FOREIGN KEY (user_id) REFERENCES User(user_id)
 );
 
-CREATE TABLE Favorite (
+CREATE TABLE Favorites (
 	favorite_id int NOT NULL,
 	task_id int,
 	user_id int,
 	PRIMARY KEY (favorite_id),
-	FOREIGN KEY (task_id) REFERENCES Task(task_id),
-	FOREIGN KEY (user_id) REFERENCES "User"(user_id)
+	FOREIGN KEY (task_id) REFERENCES Tasks(task_id),
+	FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
 
 CREATE TABLE Settings (
@@ -132,10 +132,10 @@ CREATE TABLE Settings (
 	editor_theme_dark boolean,
 	theme_dark boolean,
 	PRIMARY KEY (settings_id),
-	FOREIGN KEY (user_id) REFERENCES "User"(user_id)
+	FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
 
-CREATE TABLE Announcement (
+CREATE TABLE Announcements (
 	announcement_id int NOT NULL,
 	study_group_id int,
 	user_id int,
@@ -143,11 +143,11 @@ CREATE TABLE Announcement (
 	content text,
 	date_published Date,
 	PRIMARY KEY (announcement_id),
-	FOREIGN KEY (study_group_id) REFERENCES StudyGroup(study_group_id),
-	FOREIGN KEY (user_id) REFERENCES "User"(user_id)
+	FOREIGN KEY (study_group_id) REFERENCES StudyGroups(study_group_id),
+	FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
 
-CREATE TABLE SubmissionHistory (
+CREATE TABLE SubmissionHistories (
 	submission_history_id int NOT NULL,
 	task_id int,
 	user_id int,
@@ -155,19 +155,19 @@ CREATE TABLE SubmissionHistory (
 	created_at Date,
 	passed boolean,
 	PRIMARY KEY (submission_history_id),
-	FOREIGN KEY (task_id) REFERENCES Task(task_id),
-	FOREIGN KEY (user_id) REFERENCES "User"(user_id)
+	FOREIGN KEY (task_id) REFERENCES Tasks(task_id),
+	FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
 
 CREATE TYPE task_status AS ENUM('not_started', 'in_progress', 'completed');
-CREATE TABLE TaskStatus (
+CREATE TABLE TaskStatuses (
 	task_status_id int NOT NULL,
 	user_id int,
 	task_id int,
 	last_updated Date,
 	"status" task_status,
 	PRIMARY KEY (task_status_id),
-	FOREIGN KEY (user_id) REFERENCES "User"(user_id),
-	FOREIGN KEY (task_id) REFERENCES Task(task_id)
+	FOREIGN KEY (user_id) REFERENCES Users(user_id),
+	FOREIGN KEY (task_id) REFERENCES Tasks(task_id)
 );
 
