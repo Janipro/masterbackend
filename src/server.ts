9bfca8519a6ceb5a3670d97de4ed755990ce25e8
Jan Adrian Hafskjold <jahafskj@stud.ntnu.no>
2025-03-07 13:55:12 +0100

    Add postgraphile to backend

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,28 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-dotenv.config();
+import { postgraphile } from "postgraphile";
+import ConnectionFilterPlugin from "postgraphile-plugin-connection-filter";
 
+dotenv.config();
 const app = express();
 app.use(express.json());
 
 app.use(
   cors({
     origin: "http://localhost:5173", // Local vite
+  })
+);
+
+app.use(
+  postgraphile(process.env.DATABASE_URL, "public", {
+    watchPg: true,
+    graphiql: true,
+    enhanceGraphiql: true,
+    appendPlugins: [ConnectionFilterPlugin],
+    graphileBuildOptions: {
+      connectionFilterRelations: true,
+    },
   })
 );
 
