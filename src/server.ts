3578480c252b29f4f44163c0e5b95f087260e5b5
alexle <alexle@stud.ntnu.no>
2025-03-23 17:51:03 +0100

    set up inital help endpoint

--- src/server.ts
+++ src/server.ts
@@ -77,6 +77,52 @@ app.post("/login", async (req, res) => {
   }
 });
 
+app.post("/help", async (req, res) => {
+  const { code } = req.body;
+
+  if (!code) {
+    return res.status(400).json({ error: "No code provided" });
+  }
+
+  try {
+    const response = await fetch(
+      "https://python-runner-879157704586.europe-north1.run.app/run",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ code }),
+      }
+    );
+
+    const data = await response.json();
+    return res.json(data);
+  } catch (error) {
+    console.error("Execution Error:", error);
+    return res.status(500).json({ error: "Failed to execute code" });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { user } = req.body;
+
+  if (!user) {
+    return res.status(400).json({ error: "No user provided" });
+  }
+
+  try {
+    const response = await pool.query(
+      "SELECT * FROM users WHERE email = $1 AND password_hash = $2",
+      [user.email, user.password]
+    );
+
+    const data = response.rows;
+    return res.json(data);
+  } catch (error) {
+    console.error("Execution Error:", error);
+    return res.status(500).json({ error: "Failed to login" });
+  }
+});
+
 const PORT: number = Number(process.env.PORT || 6001);
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
