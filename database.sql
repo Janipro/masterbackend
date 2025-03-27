ed5c63f3ab93635323fb4a5d2aa20901ec0be8b0
alexle <alexle@stud.ntnu.no>
2025-03-27 23:20:39 +0100

    update database.sql

--- database.sql
+++ database.sql
@@ -1,30 +1,30 @@
 CREATE TABLE Requirements (
-    requirement_id int NOT NULL,
+    requirement_id SERIAL int NOT NULL,
 	requirement_name varchar(255),
 	PRIMARY KEY (requirement_id)
 );
 
 CREATE TABLE Classes (
-    class_id int NOT NULL,
+    class_id SERIAL int NOT NULL,
     grade int,
 	class_name varchar(10),
     PRIMARY KEY (class_id)
 );
 
 CREATE TABLE Schools (
-	school_id int NOT NULL,
+	school_id SERIAL int NOT NULL,
 	school_name varchar(255),
 	PRIMARY KEY (school_id)
 );
 
 CREATE TABLE Courses (
-    course_id int NOT NULL,
+    course_id SERIAL int NOT NULL,
     course_name varchar(255),
     PRIMARY KEY (course_id)
 );
 
 CREATE TABLE Users (
-    user_id int NOT NULL,
+    user_id SERIAL int NOT NULL,
     firstname varchar(255),
     lastname varchar(255),
     class_id int,
@@ -53,7 +53,7 @@ CREATE TABLE StudyGroups (
 );
 
 CREATE TABLE Enrolments (
-    enrolment_id int NOT NULL,
+    enrolment_id SERIAL int NOT NULL,
 	user_id int,
 	study_group_id int,
 	PRIMARY KEY (enrolment_id),
@@ -62,7 +62,7 @@ CREATE TABLE Enrolments (
 );
 
 CREATE TABLE "Access" (
-	access_id int NOT NULL,
+	access_id SERIAL int NOT NULL,
 	user_id int,
 	study_group_id int,
 	PRIMARY KEY (access_id),
@@ -71,11 +71,12 @@ CREATE TABLE "Access" (
 );
 
 CREATE TABLE Tasks (
-    task_id int NOT NULL,
+    task_id SERIAL int NOT NULL,
 	task_name varchar(255),
 	task_description varchar(255),
 	expected_code text,
 	expected_output text,
+	code_template text,
 	difficulty varchar(255),
 	"level" varchar(50),
 	"type" varchar(50),
@@ -90,7 +91,7 @@ CREATE TABLE Tasks (
 );	
 
 CREATE TABLE TaskRequirements (
-	task_requirement_id int NOT NULL,
+	task_requirement_id SERIAL int NOT NULL,
 	task_id int,
 	requirement_id int,
 	PRIMARY KEY (task_requirement_id),
@@ -98,17 +99,51 @@ CREATE TABLE TaskRequirements (
 	FOREIGN KEY (requirement_id) REFERENCES Requirements(requirement_id)
 );
 
+CREATE TABLE HelpRequests (
+	help_request_id SERIAL,
+    task_id INT NOT NULL,
+    user_id INT NOT NULL,
+    submitted_code TEXT,
+    submitted_output TEXT,
+    expected_code TEXT NOT NULL,
+    expected_output TEXT NOT NULL,
+    gpt_feedback TEXT,
+	requirement_id INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	help_request_id PRIMARY KEY,
+    FOREIGN KEY (task_id) REFERENCES Tasks(task_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+	FOREIGN KEY (requirement_id) REFERENCES Requirements(requirement_id)
+)
+
+CREATE TYPE recommended_type AS ENUM('obligatory', 'exercise');
+
 CREATE TABLE Recommended (
-    recommended_id int NOT NULL,
-	user_id int,
-	task_id int,
+	recommended_id SERIAL INT NOT NULL,
+    task_id INT NOT NULL,
+	user_id INT NOT NULL,
+    study_group_id INT NOT NULL,
+    type recommended_type NOT NULL,
+    deadline TIMESTAMP NOT NULL,
+    is_active BOOLEAN DEFAULT TRUE NOT NULL,
 	PRIMARY KEY (recommended_id),
 	FOREIGN KEY (user_id) REFERENCES Users(user_id),
-	FOREIGN KEY (task_id) REFERENCES Tasks(task_id)
+    FOREIGN KEY (task_id) REFERENCES Tasks(task_id),
+    FOREIGN KEY (study_group_id) REFERENCES StudyGroups(study_group_id)
+);
+
+CREATE TABLE RecommendedStudents (
+    recomennded_student_id SERIAL NOT NULL,
+    recommended_id INT NOT NULL,
+    user_id INT NOT NULL,
+	recomennded_student_id PRIMARY KEY,
+    FOREIGN KEY (recommended_id) REFERENCES Recommended(recommended_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    UNIQUE (recommended_id, user_id)
 );
 
 CREATE TABLE Submissions (
-    submission_id int NOT NULL,
+    submission_id SERIAL int NOT NULL,
 	task_id int,
 	user_id int,
 	submitted_code text,
@@ -120,7 +155,7 @@ CREATE TABLE Submissions (
 );
 
 CREATE TABLE Favorites (
-	favorite_id int NOT NULL,
+	favorite_id SERIAL int NOT NULL,
 	task_id int,
 	user_id int,
 	PRIMARY KEY (favorite_id),
@@ -129,7 +164,7 @@ CREATE TABLE Favorites (
 );
 
 CREATE TABLE Settings (
-    settings_id int NOT NULL,
+    settings_id SERIAL int NOT NULL,
 	user_id int,
 	editor_theme_dark boolean,
 	theme_dark boolean,
@@ -138,7 +173,7 @@ CREATE TABLE Settings (
 );
 
 CREATE TABLE Announcements (
-	announcement_id int NOT NULL,
+	announcement_id SERIAL int NOT NULL,
 	study_group_id int,
 	user_id int,
 	title varchar(100),
@@ -150,7 +185,7 @@ CREATE TABLE Announcements (
 );
 
 CREATE TABLE SubmissionHistories (
-	submission_history_id int NOT NULL,
+	submission_history_id SERIAL int NOT NULL,
 	task_id int,
 	user_id int,
 	submitted_code text,
@@ -163,7 +198,7 @@ CREATE TABLE SubmissionHistories (
 
 CREATE TYPE task_status AS ENUM('not_started', 'in_progress', 'completed');
 CREATE TABLE TaskStatuses (
-	task_status_id int NOT NULL,
+	task_status_id SERIAL int NOT NULL,
 	user_id int,
 	task_id int,
 	last_updated Date,
